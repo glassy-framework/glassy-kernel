cb5af195e58198dae5eed0bf721e6439381aa4f5
Anderson Danilo <contato@andersondanilo.com>
2020-08-22 19:12:03 -0300

    adjustments

--- src/glassy-kernel/container.cr
+++ src/glassy-kernel/container.cr
@@ -1,7 +1,11 @@
 module Glassy::Kernel
-  class Container
+  abstract class Container
     macro load_service_paths(service_paths)
       {{ run("#{__DIR__}/generate_container", service_paths.map { |s| s.resolve }.join('|')) }}
+    end
+
+    def container
+      self
     end
   end
 end
